d4887b0a0e9d52507f56fe424b2e4228bdb0e50b
sanjay892000 <sanjay892000@gmail.com>
2025-11-25 19:56:40 +0530

    js string part 1 add

---
+++ script.js
@@ -0,0 +1,57 @@
+/* let a = 4;
+let b = 7;
+let c = 8;
+
+console.table([a,b,c]) */
+
+// var keyword
+
+/* var userName;
+userName = "sanjay"
+
+userName = "shyam"
+
+var userName = 45
+console.log(userName) */
+
+//let keyword
+
+/* let age;
+
+age = 45;
+
+age = 90;
+
+let age = 25
+console.log(age) */
+
+//const keyword
+
+/* const age = 12; */
+
+/* age = 25 */
+
+/* const age = 45; */
+
+/* console.log(age) */
+
+
+/*console.log(userName)
+
+let userName = "sanjay"*/
+
+let a = 10;
+let b= 6;
+let sum = a+b;
+console.log(sum)
+
+
+console.log(a*b)
+
+
+let fname = "sanjay"
+let lname = " singh"
+let fullName = fname+lname;
+
+console.log(fullName)
+
---
+++ string.js
@@ -0,0 +1,30 @@
+/* let str = "VajraVidya"
+console.log(str)
+
+console.log(str.length) */
+
+/* let char = str.charAt(71) */
+
+/* let char = str.at(21) */
+
+/* let char = str[21]
+console.log(char)
+
+console.log(str.toUpperCase())
+
+console.log(str.toLocaleLowerCase()) */
+
+
+/* let myStr = "     Vajra   Vidya           "
+
+console.log(myStr, myStr.length)
+
+let newStr = myStr.trimStart()
+
+console.log(newStr, newStr.length)
+
+console.log(myStr.trimEnd(), myStr.trimEnd().length )
+
+console.log(myStr.trim(), myStr.trim().length) */
+
+
